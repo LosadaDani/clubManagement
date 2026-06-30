c62d79b74b800810e4a890f529c57b7eb83e4344
Dani Losada <losada.dani@gmail.com>
2026-06-30 16:25:07 +0200

    Initial Spring Boot project structure

---
+++ src/main/java/com/managementClub/managementClub/ManagementClubApplication.java
@@ -0,0 +1,13 @@
+package com.managementClub.managementClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managementClub/managementClub/ManagementClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managementClub.managementClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
