75dd54c91e116eeec6c78d483f2a5975d3888e06
Dani Losada <losada.dani@gmail.com>
2026-07-02 12:27:08 +0200

    fix create personRepositoy

---
+++ src/main/java/com/managementClub/managementClub/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.managementClub.managementClub.repository;
+
+import com.managementClub.managementClub.model.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByEmail(String email);
+}
