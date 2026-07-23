a9e703a30ddbd8b998a9a6107ec6f545cabc02c1
Dani Losada <losada.dani@gmail.com>
2026-07-23 11:44:09 +0200

    feat: create DogRepository

---
+++ src/main/java/com/managementClub/managementClub/repository/DogRepository.java
@@ -0,0 +1,16 @@
+package com.managementClub.managementClub.repository;
+
+import com.managementClub.managementClub.model.entity.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DogRepository extends JpaRepository<Dog, Long> {
+
+    Optional<Dog> findByMicrochip(String microchip);
+
+    List<Dog> findByOwner(Long ownerId);
+}
