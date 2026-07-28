ac3c995bf6ee70e8cdd6d0087cf23e3287232519
Dani Losada <losada.dani@gmail.com>
2026-07-28 11:57:04 +0200

    Docs: Finalize Dog create endpoint

--- src/main/java/com/managementClub/managementClub/controller/documentation/DogControllerDocs.java
+++ src/main/java/com/managementClub/managementClub/controller/documentation/DogControllerDocs.java
@@ -1,4 +1,52 @@
 package com.managementClub.managementClub.controller.documentation;
 
+import com.managementClub.managementClub.model.dto.DogRequestDTO;
+import com.managementClub.managementClub.model.dto.DogResponseDTO;
+import com.managementClub.managementClub.model.dto.ErrorResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(
+        name = "Perros",
+        description = "Operaciones para la gestión de perros asociados a los miembros del club"
+)
 public interface DogControllerDocs {
+
+    @Operation(
+            summary = "Crear un perro",
+            description = "Registra un nuevo perro asociado a una persona existente en el club"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",
+                    description = "Perro registrado correctamente"
+            ),
+            @ApiResponse(responseCode = "400",
+                    description = "Los datos enviados no son válidos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "No existe la persona propietaria indicada",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "409",
+                    description = "Ya existe un perro con el mismo microchip y número de pedigree",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
+    ResponseEntity<DogResponseDTO> createDog(DogRequestDTO requestDto);
+
 }
