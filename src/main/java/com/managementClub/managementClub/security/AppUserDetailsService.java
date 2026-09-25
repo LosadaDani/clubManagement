56601b049924d240f7f660c2e53de92c591c8174
Dani Losada <losada.dani@gmail.com>
2026-09-25 12:06:50 +0200

    feat: lectura de credenciales

---
+++ src/main/java/com/managementClub/managementClub/security/AppUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.managementClub.managementClub.security;
+
+import com.managementClub.managementClub.model.entity.AppUser;
+import com.managementClub.managementClub.repository.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new AppUserDetails(appUser);
+    }
+}
